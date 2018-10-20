d59a821ccd08428248c25e9aee2c2aa9a9dbffd6
Ryuya Nishino <ryuyasamaaaa@gmail.com>
2018-10-20 19:53:30 +0900

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_19_071325) do
+ActiveRecord::Schema.define(version: 2018_10_20_105229) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
