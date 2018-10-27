b95f56e1d0797f3f6f84dd8aebf8f4ca62f5acd3
Ryuya Nishino <ryuyasamaaaa@gmail.com>
2018-10-27 15:48:24 +0900

    test

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ApplicationRecord
   validates :title, length: { maximum: 32 }, presence: true
   validates :message, length: { maximum: 256 }, presence: true
   validates :name, length: { maximum: 32 }, presence: true
+  validates :weight, length: { maximum: 256}, presence: true
 end
